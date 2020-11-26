5c13b4586a13aa3a4fa42e87c21a253d643e7fa0
Michael <stall84@gmail.com>
2020-11-25 21:53:50 -0500

    Added basic setup to implement webpack later

---
+++ app.js
@@ -0,0 +1,7 @@
+"use strict";
+var button1 = document.getElementById('button1');
+function clickAlert() {
+    var alertString = 'STOP!!!  ..  In tha naaammee of LET! (before I break ur code)';
+    alert(alertString);
+}
+button1.addEventListener('click', clickAlert);
---
+++ app.ts
@@ -0,0 +1,12 @@
+
+
+const button1: any = document.getElementById('button1');
+
+function clickAlert() {
+
+    const alertString: string = 'STOP!!!  ..  In tha naaammee of LET! (before I break ur code)';
+    alert(alertString);
+   
+}
+
+button1.addEventListener('click', clickAlert);
